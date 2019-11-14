f1ad1ea64591080ec97c8b4e0bab1ba165ed4e7b
mayuxiang0929 <z5142200@ad.unsw.edu.au>
2019-11-14 13:44:42 +1100

    good

--- src/main/java/com/example/homework3/FavouriteAdapter.java
+++ src/main/java/com/example/homework3/FavouriteAdapter.java
@@ -1,16 +1,19 @@
 package com.example.homework3;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class favouriteadapter extends RecyclerView.Adapter<favouriteadapter.favouriteadapterViewHolder> {
+public class FavouriteAdapter extends RecyclerView.Adapter<FavouriteAdapter.favouriteadapterViewHolder> {
     private  static ArrayList<Catimage> breedsList1= new ArrayList<>();
     //private  static ArrayList<Breeds> breedsList2= new ArrayList<>();
 
@@ -37,13 +40,24 @@ public class favouriteadapter extends RecyclerView.Adapter<favouriteadapter.favo
     }
 
     @Override
-    public void onBindViewHolder(@NonNull favouriteadapterViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull favouriteadapterViewHolder holder, final int position) {
         if (breedsList1.isEmpty() == true) {
             holder.textView.setText("You don't have any favourite cat yet! go to add some!!");
 
         } else {
             holder.textView.setText(breedsList1.get(position).getBreeds().get(0).getName());
         }
+        holder.constraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+
+                Intent intent = new Intent(context, Catdetail.class);
+                intent.putExtra("breedsid", breedsList1.get(position).getBreeds().get(0).getId());
+                context.startActivity(intent);
+
+            }
+        });
 
     }
 
@@ -62,11 +76,14 @@ public class favouriteadapter extends RecyclerView.Adapter<favouriteadapter.favo
     public class favouriteadapterViewHolder extends RecyclerView.ViewHolder {
         public View view;
         public TextView textView;
+        public ConstraintLayout constraintLayout;
+
 
         public favouriteadapterViewHolder(@NonNull View v) {
             super(v);
             view = v;
             textView = view.findViewById(R.id.catnamee);
+            constraintLayout = v.findViewById(R.id.layout);
 
 
         }
